a2b8e9b7bb6a57899cb7b96990a13d6278269ae6
Kelly Tsai <98550592+kellstsai@users.noreply.github.com>
2022-12-06 16:29:20 -0800

    Delete Observer.java

--- Observer.java
+++
@@ -1,9 +0,0 @@
-package Patterns;
-
-public interface Observer 
-{
-    public void print(String print); 
-    public void update(String message, Subject subject);
-    public void makeMessageInNewsFeed(); 
-    public void updateUser(int user); 
-}
